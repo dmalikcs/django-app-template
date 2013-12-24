f0255fc74cc7861fbe6a8753ef13f6ceffa44cac
dmalikcs <dmalikcs@gmail.com>
2013-12-24 16:40:47 +0530

    new files added

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
-from django.db import models
+from django.db import model
+from django.form import ModelForm
 
 # Create your models here.
--- views.py
+++ views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from {{ app_name }}.models import *
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+
+def  (request):
+    template_name = ''
+    return render_to_response(
+        template_name,
+        {},
+        context_instance=RequestContext(request)
+    )
